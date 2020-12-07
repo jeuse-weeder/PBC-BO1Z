678e27114a386b2db58253e0ed339017f33660fe
jeuse-weeder <74787213+jeuse-weeder@users.noreply.github.com>
2020-12-07 07:08:40 +0100

    vectors

--- PCCZ-BO1.cpp
+++ PCCZ-BO1.cpp
@@ -2,10 +2,24 @@
 #include <iostream>
 #include <Windows.h>
 #include <synchapi.h>
+#include <vector>
 
 int main() {
 
-    LPCUWSTR gameWindow = L"Call of Duty®: BlackOps";
+    int based_value = 999999;
+    //offsets w/o pointers because COD uses mostly static addresses lmao
+    std::vector<int*> changeAddrList = {
+        (int*)0x01C0A6C8,
+        (int*)0x01A7987C,
+        (int*)0x01808F00,
+        (int*)0x01C08F10
+    };
+
+
+    DWORD based_client0 = sizeof(based_value);
+    DWORD process_ID;
+
+    LPCUWSTR gameWindow = L"Call of DutyÂ®: BlackOps";
     HWND weed = FindWindow(0, gameWindow);
     
     if (weed == 0){
@@ -13,21 +27,12 @@ int main() {
         getchar();
         exit(-1);
     }
-
-    DWORD process_ID;
     GetWindowThreadProcessId(weed, &process_ID);
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, process_ID);
     printf("Game was found\n");
     printf("Enjoy!\nLeave this program running in the background while playing\n");
-    int money = 999999;
-    int health = 999999;
-    DWORD moneyDword = sizeof(money);
-    DWORD healthDword = sizeof(health);
-    WriteProcessMemory(hProcess, (LPVOID)0x01C0A6C8, &money, moneyDword, NULL);
-    for (;;) {
-        
-        WriteProcessMemory(hProcess, (LPVOID)0x01A7987C, &health, healthDword, NULL);
-        Sleep(0xFF);
+    for (auto addr : changeAddrList) {
+        WriteProcessMemory(weed, (LPVOID)addr, &based_value, based_client0, 0);
     }
     return 0;
 }
